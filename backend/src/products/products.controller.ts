2b402bc62f9ac872b62ec61b8d3f5562aed86aa8
Su <hoanganhng293@gmail.com>
2026-03-07 00:58:15 +0700

    add products service

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,28 +1,58 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { Permissions } from 'src/common/decorators/permissions.decorator';
 import { CreateProductDto } from './dto/create-product.dto';
 import { PermissionsGuard } from 'src/common/guards/permissions.guard';
+import { QueryProductDto } from './dto/query-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  //---PUBLIC ROUTES-------------------------
+
   @Get()
-  getProductList() {
-    return this.productsService.findAll();
+  findAll(@Query() query: QueryProductDto) {
+    return this.productsService.findAll(query);
   }
 
   @Get(':id')
-  getProduct(@Param('id') id: string) {
+  findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
+
+  //---ADMIN ROUTES-------------------------
 
   @UseGuards(JwtAuthGuard, PermissionsGuard)
   @Permissions('product.create')
   @Post()
   create(@Body() dto: CreateProductDto) {
     return this.productsService.create(dto);
+  }
+
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions('product.update')
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
+    return this.productsService.update(+id, dto);
+  }
+
+  @UseGuards(JwtAuthGuard, PermissionsGuard)
+  @Permissions('product.delete')
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productsService.softRemove(+id);
   }
 }
